9562befde66753bb1baf08b4f720fa1e26b0da9e
ishanpote <160637054+ishanpote@users.noreply.github.com>
2026-03-02 15:14:53 +0530

    code changed

--- Pract6.py
+++ Pract6.py
@@ -1,6 +1,6 @@
 # GROUP 1 - Compute semantic similarity between two sentences using embeddings or vector-based representations.
 
-"""from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 sentence1 = input("Enter first sentence: ")
@@ -13,7 +13,7 @@ similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
 
 print("\nSemantic Similarity Score:", similarity[0][0])
 
-"""
+
 """
 # Semantic Similarity with User Input
 # Embedding-based + lightweight POS adjustment
